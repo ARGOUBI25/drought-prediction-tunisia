dd85b998332089d821acd866c3fc8262c5931169
ARGOUBI25 <mejdiargoubi@yahoo.fr>
2026-02-22 16:47:37 +0100

    Create compute_ssmi.py in src/data

---
+++ src/data/compute_ssmi.py
@@ -0,0 +1,9 @@
+# Function to compute the SSMI
+
+def compute_ssmi(data):
+    """
+    Computes the Soil Moisture Supply Index (SSMI)
+    from input data.
+    """
+    # Implement the SSMI computation
+    pass
